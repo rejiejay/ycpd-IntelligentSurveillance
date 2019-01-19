c8a9c9f4a2d89c4aae4c0fb47f9233c0b9efae24
rejiejay <454766952@qq.com>
2019-01-19 16:00:41 +0800

    rule-edit

---
+++ src/api/system/rule-edit.js
@@ -0,0 +1,90 @@
+import config from '@/config';
+import apibasics from '@/components/apibasics';
+import notFilter from '@/components/apibasics-notFilter';
+
+/**
+ * 新增预警
+ * @param {string} alarmName  预警名称
+ * @param {string} indicatorType  预警指标
+ * @param {string} objType  预警对象
+ * @param {string} areaType  预警范围
+ * @param {string} frequency  通知时间
+ * @param {string} activeType  通知有效期
+ * @param {string} startDate 通知有效期
+ * @param {string} endDate 通知有效期
+ * @param {string} userIds 预警通知人 [id, id, id, id]
+ */
+export function addAlarmRuleUsingPOST(alarmName, indicatorType, objType, areaType, frequency, activeType, startDate, endDate, userIds) {
+    return notFilter({
+        url: `${config.url.origin}/cdimms/server/alarmRule/addAlarmRule`,
+        method: 'post',
+        headers: {'Content-Type': 'application/json'},
+        data: {
+            alarmName: alarmName, 
+            indicatorType: indicatorType, 
+            objType: objType, 
+            areaType: areaType, 
+            frequency: frequency, 
+            activeType: activeType, 
+            startDate: startDate,
+            endDate: endDate,
+            userIds: userIds,
+        }
+    });
+}
+
+/**
+ * 修改预警
+ * @param {string} id 预警唯一标识
+ * @param {string} alarmName  预警名称
+ * @param {string} indicatorType  预警指标
+ * @param {string} objType  预警对象
+ * @param {string} areaType  预警范围
+ * @param {string} frequency  通知时间
+ * @param {string} activeType  通知有效期
+ * @param {string} startDate 通知有效期
+ * @param {string} endDate 通知有效期
+ * @param {string} userIds 预警通知人 [id, id, id, id]
+ */
+export function modifyAlarmRuleUsingPOST(id, alarmName, indicatorType, objType, areaType, frequency, activeType, startDate, endDate, userIds) {
+    return notFilter({
+        url: `${config.url.origin}/cdimms/server/alarmRule/modifyAlarmRule`,
+        method: 'post',
+        headers: {'Content-Type': 'application/json'},
+        data: {
+            id: id, 
+            alarmName: alarmName, 
+            indicatorType: indicatorType, 
+            objType: objType, 
+            areaType: areaType, 
+            frequency: frequency, 
+            activeType: activeType, 
+            startDate: startDate,
+            endDate: endDate,
+            userIds: userIds,
+        }
+    });
+}
+
+/**
+ * 预警通知人下拉框
+ */
+export function queryUserNameUsingGET() {
+    return apibasics({
+        url: `${config.url.origin}/cdimms/server/alarmRule/queryUserName`,
+        method: 'get',
+        headers: {'Content-Type': 'application/json'},
+    });
+}
+
+/**
+ * 查询单个告警规则信息
+ * @param {string} id 预警唯一标识
+ */
+export function queryAlarmRuleUsingGET(id) {
+    return apibasics({
+        url: `${config.url.origin}/cdimms/server/alarmRule/queryAlarmRule?id=${id}`,
+        method: 'get',
+        headers: {'Content-Type': 'application/json'},
+    });
+}
--- src/views/system/rule-edit.vue
+++ src/views/system/rule-edit.vue
@@ -57,7 +57,12 @@
             <el-col :span="16">
                 <div class="details-form-item">
                     <div class="form-item-title">预警通知人</div>
-                    <el-select v-model="ruleInfoPeopleSection" multiple placeholder="预警通知人">
+                    <el-select 
+                        v-model="ruleInfoPeopleSection" 
+                        multiple 
+                        placeholder="预警通知人"
+                        :disabled="ruleRangeSection !== '1'"
+                    >
                         <el-option
                             v-for="item in ruleInfoPeopleOptions"
                             :key="item.value"
@@ -73,11 +78,11 @@
             <el-col :span="8">
                 <div class="details-form-item">
                     <div class="form-item-title">通知时间<span>ps：每月1日 09:00 短信通知</span></div>
-                    <el-select v-model="ruleRangeSection" placeholder="通知时间">
-                        <el-option label="每天" value="每天"></el-option>
-                        <el-option label="每三天" value="每三天"></el-option>
-                        <el-option label="每周" value="每周"></el-option>
-                        <el-option label="每月" value="每月"></el-option>
+                    <el-select v-model="ruleFrequencySection" placeholder="通知时间">
+                        <el-option label="每天" value="0"></el-option>
+                        <el-option label="每三天" value="1"></el-option>
+                        <el-option label="每周" value="2"></el-option>
+                        <el-option label="每月" value="3"></el-option>
                     </el-select>
                 </div>
             </el-col>
@@ -88,13 +93,14 @@
                     <el-row>
                         <el-col :span="11">
                             <el-select v-model="ruleValiditySection" placeholder="通知有效期">
-                                <el-option label="永久" value="永久"></el-option>
-                                <el-option label="自定义" value="自定义"></el-option>
+                                <el-option label="永久" value="0"></el-option>
+                                <el-option label="自定义" value="1"></el-option>
                             </el-select>
                         </el-col>
                         <el-col :span="1"><div>&nbsp;</div></el-col>
                         <el-col :span="12">
                             <el-date-picker
+                                :disabled="ruleValiditySection !== '1'"
                                 v-model="startendTime"
                                 type="daterange"
                                 align="right"
@@ -113,15 +119,19 @@
 
     <div class="user-edit-operate flex-center">
         <div class="details-operate-container flex-start">
-            <el-button type="info" plain>取消</el-button>
+            <el-button type="info" plain @click="$router.back(-1)">取消</el-button>
             <div style="width: 45px;"></div>
-            <el-button type="primary">保存</el-button>
+            <el-button type="primary" @click="submit">保存</el-button>
         </div>
     </div>
 </div>
 </template>
 
 <script>
+// 请求类
+import { addAlarmRuleUsingPOST, queryUserNameUsingGET, modifyAlarmRuleUsingPOST, queryAlarmRuleUsingGET } from "@/api/system/rule-edit";
+// 组件类
+import TimeConver from '@/utils/TimeConver';
 
 export default {
     name: 'user-edit',
@@ -166,43 +176,53 @@ export default {
              */
             pageType: 'add',
             
+            ruleId: '', // 预警唯一标识
             ruleName: '', // 预警名称
             ruleStandardSection: '', // 预警指标
             ruleStandardOptions: [
                 {
-                    value: '预警指标一',
-                    label: '预警指标一',
+                    value: '0',
+                    label: '保费',
+                }, {
+                    value: '1',
+                    label: '定损',
+                }, {
+                    value: '2',
+                    label: '产保比',
                 }
             ],
 
             ruleTargetSection: '', // 预警对象
             ruleTargetOptions: [
                 {
-                    value: '预警指标一',
-                    label: '预警指标一',
+                    value: '0',
+                    label: '支公司',
+                }, {
+                    value: '1',
+                    label: '车行',
                 }
             ],
 
             ruleRangeSection: '', // 预警范围
             ruleRangeOptions: [
                 {
-                    value: '预警指标一',
-                    label: '预警指标一',
+                    value: '0',
+                    label: '全部',
+                }, {
+                    value: '1',
+                    label: '自定义',
                 }
             ],
 
             ruleInfoPeopleSection: '', // 预警通知人
             ruleInfoPeopleOptions: [
-                {
-                    value: '预警指标一',
-                    label: '预警指标一',
-                }, {
-                    value: '预警指标er',
-                    label: '预警指标er',
-                }
+                // {
+                //     value: '预警通知人',
+                //     label: '预警通知人',
+                // },
             ],
 
-            ruleRangeSection: '', // 通知时间
+            ruleFrequencySection: '', // 通知时间
             ruleValiditySection: '', // 通知有效期
 
             startendTime: [ // 通知有效期
@@ -215,6 +235,7 @@ export default {
 
 	mounted: function mounted() {
         this.initPageData(); // 初始化页面数据
+        this.queryUserName(); // 初始化 预警通知人
     },
 
 	methods: {
@@ -224,9 +245,145 @@ export default {
          */
         initPageData: function initPageData() {
             // 如果 页面状态 存在 id 表示编辑状态
-            if (this.$route.query.id) {
+            let ruleId = this.$route.query.id
+            if (ruleId) {
                 this.pageType = 'edit';
+                this.ruleId = ruleId;
+
+                queryAlarmRuleUsingGET(ruleId)
+                .then(val => {
+                    console.log('查询单个告警规则信息', val);
+
+                    this.ruleName = val.alarmName ? val.alarmName : ''; //  预警名称
+                    this.ruleStandardSection = val.indicatorType ? `${val.indicatorType}` : ''; //  预警指标
+                    this.ruleTargetSection = val.objType ? `${val.objType}` : ''; //  预警对象
+                    this.ruleRangeSection = val.areaType ? `${val.areaType}` : ''; //  预警范围
+                    this.ruleFrequencySection = val.frequency ? `${val.frequency}` : ''; //  通知时间
+                    this.ruleValiditySection = val.activeType ? `${val.activeType}` : ''; //  通知有效期
+                    if (val.startDate && val.endDate) {
+                        this.startendTime[0] = new Date(TimeConver.YYYYmmDDToTimestamp(val.startDate)); //  通知有效期
+                        this.startendTime[1] = new Date(TimeConver.YYYYmmDDToTimestamp(val.endDate)); //  通知有效期
+                    }
+                    this.ruleInfoPeopleSection = val.userIds ? val.userIds : []; // 预警通知人 [id, id, id, id]
+
+                }, error => console.log(error));
             }
+        },
+
+        /**
+         * 预警通知人
+         */
+        queryUserName: function queryUserName() {
+            const _this  = this;
+
+            queryUserNameUsingGET()
+            .then(val => {
+                let data = val.data;
+
+                if (data && data instanceof Array && data.length > 0) {
+                    _this.ruleInfoPeopleOptions = data.map(item => ({
+                        value: item[0],
+                        label: item[1],
+                    }));
+                }
+
+            }, error => console.log(error))
+        },
+        /**
+         * 新增预警
+         */
+        submit: function submit() {
+            const _this = this;
+
+            if (!this.ruleName) {
+                return this.$notify({title: '提示', message: '预警名称不能为空', duration: 0 });
+            }
+            if (!this.ruleStandardSection) {
+                return this.$notify({title: '提示', message: '预警指标不能为空', duration: 0 });
+            }
+            if (!this.ruleTargetSection) {
+                return this.$notify({title: '提示', message: '预预警对象不能为空', duration: 0 });
+            }
+            if (!this.ruleRangeSection) {
+                return this.$notify({title: '提示', message: '预警范围不能为空', duration: 0 });
+            }
+            if (this.ruleRangeSection === '1' && !this.ruleInfoPeopleSection) {
+                return this.$notify({title: '提示', message: '预警通知人不能为空', duration: 0 });
+            }
+            if (!this.ruleFrequencySection) {
+                return this.$notify({title: '提示', message: '通知时间不能为空', duration: 0 });
+            }
+            if (!this.ruleValiditySection) {
+                return this.$notify({title: '提示', message: '通知有效期不能为空', duration: 0 });
+            }
+            if (this.ruleValiditySection === '1' && !this.startendTime[0] && !this.startendTime[1]) {
+                return this.$notify({title: '提示', message: '通知有效期不能为空', duration: 0 });
+            }
+
+            let id = this.ruleId; // 预警唯一标识
+            let alarmName = this.ruleName; //  预警名称
+            let indicatorType = this.ruleStandardSection; //  预警指标
+            let objType = this.ruleTargetSection; //  预警对象
+            let areaType = this.ruleRangeSection; //  预警范围
+            let frequency = this.ruleFrequencySection; //  通知时间
+            let activeType = this.ruleValiditySection; //  通知有效期
+
+            // 自定义的情况下
+            let startDate = ''; // 通知有效期
+            let endDate = ''; // 通知有效期
+            if (activeType === '1') {
+                startDate = `${TimeConver.dateToFormat(this.startendTime[0])} 00:00:00`; // 通知有效期
+                endDate = `${TimeConver.dateToFormat(this.startendTime[1])} 00:00:00`; // 通知有效期
+            }
+            let userIds = this.ruleInfoPeopleSection; // 预警通知人 [id, id, id, id]
+
+            let addAlarmRule = () => {
+
+                addAlarmRuleUsingPOST(alarmName, indicatorType, objType, areaType, frequency, activeType, startDate, endDate, userIds)
+                .then(val => {
+                    if (val.code === 1000) {
+                        _this.$message({
+                            showClose: true,
+                            message: '添加成功',
+                            type: 'success'
+                        });
+                        _this.$router.back(-1);
+                        
+                    } else if (val.code === 1001) {
+                        return _this.$alert('添加告警规则异常', '添加失败');
+                    } else {
+                        return _this.$alert(val.msg, '添加失败');
+                    }
+
+                }, error => console.log(error));
+            }
+
+            let modifyAlarmRule = () => {
+                modifyAlarmRuleUsingPOST(id, alarmName, indicatorType, objType, areaType, frequency, activeType, startDate, endDate, userIds)
+                .then(val => {
+                    if (val.code === 1000) {
+                        _this.$message({
+                            showClose: true,
+                            message: '修改成功',
+                            type: 'success'
+                        });
+                        _this.$router.back(-1);
+                        
+                    } else if (val.code === 1001) {
+                        return _this.$alert('修改告警规则异常', '修改失败');
+                    } else {
+                        return _this.$alert(val.msg, '修改失败');
+                    }
+
+                }, error => console.log(error));
+            }
+
+            if (this.pageType === 'add') {
+                addAlarmRule()
+            } else {
+                modifyAlarmRule();
+            }
+
         },
 
         /**
